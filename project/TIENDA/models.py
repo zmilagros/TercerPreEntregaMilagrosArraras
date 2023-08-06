b4a075bec6ea39f00447f4765bccaf5c6d534aa0
Milagros Arraras <arrarasmilagros@gmail.com>
2023-08-06 17:19:12 -0300

    creada la app clientes y la app productos

--- project/TIENDA/models.py
+++ project/TIENDA/models.py
@@ -2,24 +2,6 @@ from django.db import models
 
 # Create your models here.
 
-class Cliente(models.Model):
-    nombre=models.CharField(max_length=50)
-    apellido=models.CharField(max_length=50)
-    email=models.EmailField()
-
-    def __str__(self):
-        return f"{self.nombre} {self.apellido}"
-
-    
-class Producto(models.Model):
-    descripcion=models.CharField(max_length=50)
-    talle=models.CharField(max_length=50, default="")
-    precio=models.FloatField()
-
-    def __str__(self):
-        return f" Descripción: {self.descripcion} - Talle: {self.talle} - Precio: {self.precio}"
-    
-
 
 class Categoria(models.Model):
     CATEGORIAS = [
