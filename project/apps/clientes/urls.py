240cfe4d402e56fc64a106787ce0ad8e9c02a271
Milagros Arraras <arrarasmilagros@gmail.com>
2023-08-10 12:27:38 -0300

    cambios en el nav

--- project/apps/clientes/urls.py
+++ project/apps/clientes/urls.py
@@ -12,5 +12,4 @@ urlpatterns = [
     path('editar/', editar_usuario, name='usuarios-editar'),
     path('logout/', LogoutView.as_view(template_name='clientes/logout.html'), name='usuarios-logout'),
     path('cargar_avatar/', AvatarUploadView.as_view(), name='cargar_avatar'),
-
 ]
