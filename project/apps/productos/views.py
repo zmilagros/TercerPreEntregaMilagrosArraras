2f21cfb92996f766794f7cf8fb63774c45d2ad24
Milagros Arraras <arrarasmilagros@gmail.com>
2023-08-09 23:21:10 -0300

    views.py modificado de producto

--- project/apps/productos/views.py
+++ project/apps/productos/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
-from .models import Producto
 from django.urls import reverse_lazy
-from .forms import ProductoForm
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 from django.views.generic import (
     CreateView,
     DeleteView,
@@ -9,24 +9,25 @@ from django.views.generic import (
     ListView,
     UpdateView,
 )
-from django.contrib.auth.mixins import LoginRequiredMixin
-from . import forms, models
+from .models import Producto
+from .forms import ProductoForm
 
-
-class ProductoList(LoginRequiredMixin, ListView):
+@method_decorator(login_required, name='dispatch')
+class ProductoList(ListView):
     model = Producto
     template_name = "productos/products.html"
     context_object_name = "productos"
     form_class = ProductoForm
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form'] = ProductoForm()
+        return context
+
     def post(self, request, *args, **kwargs):
         form = self.form_class(request.POST)
         if form.is_valid():
-            producto = Producto()
-            producto.descripcion = form.cleaned_data['descripcion']
-            producto.talle = form.cleaned_data['talle']
-            producto.precio = form.cleaned_data['precio']
-            producto.save()
+            form.save()
             form = self.form_class()
         else:
             form = self.form_class()
@@ -35,27 +36,23 @@ class ProductoList(LoginRequiredMixin, ListView):
         context = {"productos": productos, "form": form}
         return self.render_to_response(context)
 
-
 class ProductoCreate(CreateView):
-    model = models.Producto
-    form_class = forms.ProductoForm
+    model = Producto
+    form_class = ProductoForm
     template_name = "productos/crearproducto.html"
     success_url = reverse_lazy("productos:lista_productos")
-
 
 class ProductoDetail(DetailView):
     template_name = "productos/detalleproducto.html"
-    model = models.Producto
-
+    model = Producto
 
 class ProductoUpdate(UpdateView):
-    model = models.Producto
-    form_class = forms.ProductoForm
+    model = Producto
+    form_class = ProductoForm
     template_name = "productos/crearproducto.html"
     success_url = reverse_lazy("productos:lista_productos")
 
-
 class ProductoDelete(DeleteView):
-    model = models.Producto
+    model = Producto
     template_name = "productos/eliminarproducto.html"
     success_url = reverse_lazy("productos:lista_productos")
