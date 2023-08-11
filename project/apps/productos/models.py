8259e55094e500a50746aabe44cf05a884d98219
Milagros Arraras <arrarasmilagros@gmail.com>
2023-08-11 11:49:20 -0300

    card, tarjetas agregadas

--- project/apps/productos/models.py
+++ project/apps/productos/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 
 class Producto(models.Model):
-    descripcion=models.CharField(max_length=50)
-    talle=models.CharField(max_length=50, default="")
-    precio=models.FloatField()
+    descripcion = models.CharField(max_length=50)
+    talle = models.CharField(max_length=50, default="")
+    precio = models.FloatField()
+    imagen = models.ImageField(upload_to='productos/', blank=True, null=True)  # Agrega este campo
 
     def __str__(self):
-        return f" Descripción: {self.descripcion} - Talle: {self.talle} - Precio: {self.precio}"
+        return f"Descripción: {self.descripcion} - Talle: {self.talle} - Precio: {self.precio}"
